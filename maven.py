ec5bee57769b8f3b4d238015664655290e96e90d
AdityaHitachi <aditya.gadekar@hitachivantara.com>
2023-05-16 19:37:26 +0530

    update

--- maven.py
+++ maven.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 def find_parent_pom_directory_for_all_changed_files(changed_files): 
     parent_dirs = {}
     for changed_file in changed_files:
@@ -13,11 +15,12 @@ def find_parent_pom_directory_for_all_changed_files(changed_files):
             parent_dirs[changed_file] = None
     return parent_dirs
 
-changed_files = ["/Users/agadekar/Desktop/LDOS/data-plane/data-plane/repository/common/src/main/java/com/hitachivantara/data/plane/repository/IRepositoryEndpoints.java","C:/Users/agadekar/Desktop/LDOS/data-plane/data-plane/repository/jdbc/src/test/resources/sql_lite.db"]
+path = sys.argv[1]
+changed_files = [path]
+print(f" Changed file is - {changed_files} ")
 parent_dirs = find_parent_pom_directory_for_all_changed_files(changed_files)
 for changed_file, parent_dir in parent_dirs.items():
     if parent_dir:
         print(f"The parent directory containing the pom.xml file for {changed_file} is: {parent_dir}")
     else:
         print(f"No pom.xml file was found in any parent directory for {changed_file}.")
-        
