9603e15cd77b0eb80d11f7de1012e30b94dc380e
AdityaHitachi <aditya.gadekar@hitachivantara.com>
2023-05-17 12:14:52 +0530

    update

--- maven.py
+++ maven.py
@@ -22,11 +22,16 @@ changed_files = [path_list]
 # changed_files_exclude_yaml = [f for f in changed_files if not f.endswith(".yaml") and not f.endswith(".yml") or not f.startswith(".github/workflows/")]
 # changed_files_exclude_yaml = [f for f in changed_files if not f.endswith(".yaml") and not f.endswith(".yml")]
 print(f" Changed file is - {changed_files} ")
-parent_dirs = find_parent_pom_directory_for_all_changed_files(*changed_files)
 
-for changed_file, parent_dir in parent_dirs.items():
-    if parent_dir:
-        print(f"The parent directory containing the pom.xml file for {changed_file} is: {parent_dir}")
-        print(f"::set-output name=parent_dir::{parent_dir}")
-    else:
-        print(f"No pom.xml file was found in any parent directory for {changed_file}.")
+for changed_files_exclude_yaml in changed_files:
+    if isinstance(changed_files_exclude_yaml, str) and not changed_files_exclude_yaml.endswith(".yaml") and not changed_files_exclude_yaml.endswith(".yml") or not changed_files_exclude_yaml.startswith(".github/workflows/"):
+        print(changed_files_exclude_yaml)
+
+# parent_dirs = find_parent_pom_directory_for_all_changed_files(*changed_files)
+
+# for changed_file, parent_dir in parent_dirs.items():
+#     if parent_dir:
+#         print(f"The parent directory containing the pom.xml file for {changed_file} is: {parent_dir}")
+#         #print(f"::set-output name=parent_dir::{parent_dir}")
+#     else:
+#         print(f"No pom.xml file was found in any parent directory for {changed_file}.")
