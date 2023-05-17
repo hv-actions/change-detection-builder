ff793928efafd9382f74c5ff19c044ae30b8f771
AdityaHitachi <aditya.gadekar@hitachivantara.com>
2023-05-17 12:49:35 +0530

    update

--- maven.py
+++ maven.py
@@ -20,15 +20,8 @@ print(f"Path is - {path}")
 changed_files = path.split()
 changed_files_exclude_yaml = [f for f in changed_files if not f.endswith(".yaml") and not f.endswith(".yml") or not f.startswith(".github/workflows/")]
 changed_files_exclude_yaml = [changed_files_exclude_yaml]
-# changed_files_exclude_yaml = [f for f in changed_files if not f.endswith(".yaml") and not f.endswith(".yml")]
 print(f" Changed file is excluding yaml - {changed_files_exclude_yaml} ")
-
-# for changed_files_exclude_yaml in changed_files:
-#     if isinstance(changed_files_exclude_yaml, str) and not changed_files_exclude_yaml.endswith(".yaml") and not changed_files_exclude_yaml.endswith(".yml") or not changed_files_exclude_yaml.startswith(".github/workflows/"):
-#         print(changed_files_exclude_yaml)
-
 parent_dirs = find_parent_pom_directory_for_all_changed_files(*changed_files_exclude_yaml)
-print(parent_dirs)
 for changed_file, parent_dir in parent_dirs.items():
     if parent_dir:
         print(f"The parent directory containing the pom.xml file for {changed_file} is: {parent_dir}")
